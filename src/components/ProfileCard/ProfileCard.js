e3e710ff755d69b3d3de21435baea2f7d27e01ca
Theodore Moreland <theodoremoreland@yahoo.com>
2020-11-30 00:24:20 -0600

    Adding grid to ProfileCard and overflow-y to timeline

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -11,22 +11,29 @@ import Timeline from '../Timeline/Timeline';
 // Custom styles
 import './ProfileCard.css';
 import profilePic from '../../images/headshot-1.jpg';
+import { Col, Row } from 'react-bootstrap';
 
 
 export default function ProfileCard() {
 
     return (
         <Card className="profileCard">
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                <Image className="profilePic" src={profilePic} roundedCircle fluid/>
+            <Row>
+            <Col className="profileContainer" xs={3}>
+                <Card.Body>
+                    <Card.Title className="profileTitle">Theodore Moreland</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted profileSubTitle">Software Engineer</Card.Subtitle>
+                    <Image className="profilePic" src={profilePic} roundedCircle fluid/>
+                    <Card.Text className="text-muted bio">
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Col>
+            <Col className="timelineContainer" xs={8}>
                 <Timeline />
-                <Card.Text className="bio">
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                </Card.Text>
-            </Card.Body>
+            </Col>
+            </Row>
         </Card>
     );
 };
