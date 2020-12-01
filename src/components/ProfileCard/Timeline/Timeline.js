a067798ad5b8f9fd5428e76f0f8ae5b4ddf5ce1f
Theodore Moreland <theodoremoreland@yahoo.com>
2020-12-01 01:01:42 -0600

    Resizing

--- src/components/ProfileCard/Timeline/Timeline.js
+++ src/components/ProfileCard/Timeline/Timeline.js
@@ -1,32 +1,20 @@
 // React
 import React from 'react';
 
-// Bootstrap
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+// Custom Components
+import ProjectCard from '../ProjectCard/ProjectCard';
 
 // Custom styles
 import './Timeline.css';
 
 
-export default function ProfileCard(props) {
+export default function Timeline(props) {
     const {content} = props;
 
     return (
         <div className="timeline">
             {   content !== undefined
-                    ? content.map(repo =>
-                        <Card className="projectCard">
-                        <Card.Img variant="top" src={repo.image} />
-                        <Card.Body>
-                            <Card.Title>{repo.name}</Card.Title>
-                            <Card.Text>
-                            
-                            {repo.desc}
-                            </Card.Text>
-                            <Button variant="primary">View on GitHub</Button>
-                        </Card.Body>
-                        </Card>)
+                    ? content.map(repo => <ProjectCard repo={repo} />)
                     : ""
             }
         </div>
