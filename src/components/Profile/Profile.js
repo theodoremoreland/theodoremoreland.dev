5be630d75b53f8efd9c0a228ed7fe3ed4f450161
Theodore Moreland <theodoremoreland@yahoo.com>
2020-12-06 00:59:46 -0600

    Added GitHub and LinkedIn icons + links

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,10 +4,13 @@ import React from 'react';
 // Bootstrap
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
+import { Col, Row } from 'react-bootstrap';
 
 // Custom styles
 import './Profile.css';
 import profilePic from '../../images/headshot-1.jpg';
+import octocatIcon from '../../images/Octocat.png';
+import linkedInIcon from '../../images/LI-In-Bug.png';
 
 export default function Profile() {
 
@@ -15,10 +18,22 @@ export default function Profile() {
         <Card className="profile">
             <Card.Body>
                 <Card.Title className="profileTitle">Theodore Moreland</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted profileSubTitle">Software Engineer</Card.Subtitle>
+                <Card.Subtitle className="mb-2 profileSubTitle">Software Engineer</Card.Subtitle>
                 <Image className="profilePic" src={profilePic} roundedCircle fluid/>
-                <Card.Text className="text-muted profileBio">
-                    @github.theodoremoreland
+                <Row className="profileLinks" noGutters>
+                    <Col>
+                        <a href="https://github.com/theodoremoreland" rel="noopener noreferrer" target="_blank">
+                            <Image className="octocatIcon" src={octocatIcon} fluid/>
+                        </a>
+                    </Col>
+                    <Col>
+                        <a href="https://www.linkedin.com/in/theodore-moreland/" rel="noopener noreferrer" target="_blank">
+                            <Image className="linkedInIcon" src={linkedInIcon} fluid/>
+                        </a>   
+                    </Col>
+                </Row>
+                <Card.Text className="profileBio">
+                    Some stuff about me.
                 </Card.Text>
             </Card.Body>
         </Card>
