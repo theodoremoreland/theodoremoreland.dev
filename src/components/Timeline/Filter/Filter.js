9e171f7b61ac9879ec3d5a4e4d8d5c4e41c9b46f
Theodore Moreland <theodoremoreland@yahoo.com>
2020-12-05 19:01:44 -0600

    Completed basic functionality of filtering porjects

--- src/components/Timeline/Filter/Filter.js
+++ src/components/Timeline/Filter/Filter.js
@@ -1,45 +1,81 @@
 // React
-import React, { useState } from 'react';
+import React from 'react';
 
 // Bootstrap
 import Form from 'react-bootstrap/Form';
 import ReactWordcloud from 'react-wordcloud';
 
-
 // Custom styles
 import './Filter.css';
 
-export default function Filter(props) {
-    const { label, wordCloudData } = props;
-    const [showWordCloud, setShowWordCloud] = useState(false);
+let filtersToAdd = new Set();
+
+export default function Filter({filterComponentData}) {
+    const { label
+        , wordCloudData
+        , filtersCurrentlyInUse
+        , setFiltersCurrentlyInUse
+        , wordCloudsAreOpenObject
+        , setWordCloudsAreOpenObject
+    } = filterComponentData;
+    const topicsForLabel = wordCloudData.map((data) => data.text);
+    const userSelectedTopicsForLabelString = [...filtersToAdd].filter((topic) => topicsForLabel.includes(topic)).join();
+    
+    const addOrRemoveTopicFromFilter = (userSelectedTopic) => {
+        if (filtersToAdd.has(userSelectedTopic)) {
+            filtersToAdd.delete(userSelectedTopic);
+            filtersCurrentlyInUse.delete(userSelectedTopic);
+            setFiltersCurrentlyInUse(new Set([...filtersCurrentlyInUse, ...filtersToAdd]));
+        }
+        else {
+            filtersToAdd.add(userSelectedTopic);
+            setFiltersCurrentlyInUse(new Set([...filtersCurrentlyInUse, ...filtersToAdd]));
+        }
+    };
 
     const options = {
         rotations: 0,
         padding: 0,
-        fontSizes: [15, 65]
-      };
+        fontSizes: [12, 82],
+        fontFamily: "header"
+    };
+
+    const callbacks = {
+        onWordClick: (word) => addOrRemoveTopicFromFilter(word.text)
+    }
 
     const handleClick = () => {
-        setShowWordCloud(!showWordCloud);
+        const shouldOpenWordCloud = !wordCloudsAreOpenObject[label];
+
+        if (shouldOpenWordCloud) {
+            // The following statement closes all wordclouds in shallow copy
+            Object.keys(wordCloudsAreOpenObject).forEach((key) => wordCloudsAreOpenObject[key] = false);
+            // The next statement sets shallow copy, then opens active wordcloud
+            setWordCloudsAreOpenObject({...wordCloudsAreOpenObject, [label] : true});
+        }
+        else {
+            setWordCloudsAreOpenObject({...wordCloudsAreOpenObject, [label] : false});
+        }
     };
 
     return (
         <>
             <Form className="filter">
-                <Form.Group controlId="fella" onClick={() => handleClick()}>
+                <Form.Group controlId={`${label.toLowerCase()}FormGroup`} onClick={() => handleClick()}>
                 <Form.Label>{label}</Form.Label>
                 <Form.Control
                     as="input"
                     custom
-                    defaultValue={`Every ${label.toLowerCase()}`} 
+                    value={userSelectedTopicsForLabelString || `All ${label.toLowerCase()}`} 
                 />
                 </Form.Group>
             </Form>
-            { showWordCloud
-                ?   <div id="wordCloudCanvas">
+            { wordCloudsAreOpenObject[label]
+                ?   <div className="wordCloudCanvas" id={`${label.toLowerCase()}WordCloudCanvas`}>
                         <ReactWordcloud
                             words={wordCloudData}
                             options={options}
+                            callbacks={callbacks}
                         />
                     </div>
                 : ""
--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,5 +1,5 @@
 // React
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 // Bootstrap
 import { Col, Row } from 'react-bootstrap';
@@ -14,28 +14,64 @@ import Filter from './Filter/Filter';
 // Custom styles
 import './Timeline.css';
 
+const filterLabels = [
+    "Contexts",
+    "Competencies",
+    "Languages",
+    "Tools"
+];
+
+const createWordCloudsAreOpenObject = () => {
+    const wordCloudsAreOpenObject = {};
+    for (const label of filterLabels) {
+        wordCloudsAreOpenObject[label] = false;
+    };
+    return wordCloudsAreOpenObject;
+};
+
 export default function Timeline(props) {
     const { projects } = props;
-    const { contexts, competencies, languages, tools } = useMemo(() => createWordCloudData(projects), [projects]);
+    const [filtersCurrentlyInUse, setFiltersCurrentlyInUse] = useState([]);
+    const wordCloudData = useMemo(() => createWordCloudData(projects), [projects]);
+    const [wordCloudsAreOpenObject, setWordCloudsAreOpenObject] = useState(createWordCloudsAreOpenObject);
+
+    const filterTopics = useCallback(() => {
+        if ([...filtersCurrentlyInUse].length > 0) {
+            let filteredProjectsSet = new Set();
+            for (const topic of filtersCurrentlyInUse) {
+                const filteredProjectsArray = projects.filter(project => project.topics.includes(topic));
+                filteredProjectsArray.forEach(project => filteredProjectsSet.add(project));
+            }
+            return [...filteredProjectsSet];
+        }
+        else {
+            return projects;
+        }
+    }, [projects, filtersCurrentlyInUse]);
 
     return (
         <>
             <Row>
-                <Col>
-                    <Filter label={"Context"} wordCloudData={contexts} />
-                </Col>
-                <Col>
-                    <Filter label={"Competency"} wordCloudData={competencies} />
-                </Col>
-                <Col>
-                    <Filter label={"Language"} wordCloudData={languages} />
-                </Col>
-                <Col>
-                    <Filter label={"Tool"} wordCloudData={tools} />
-                </Col>
+                {
+                    filterLabels.map((label) => {
+                        const filterComponentData = {
+                            label
+                            , wordCloudData : wordCloudData[label.toLowerCase()]
+                            , filtersCurrentlyInUse
+                            , setFiltersCurrentlyInUse
+                            , wordCloudsAreOpenObject
+                            , setWordCloudsAreOpenObject
+                        };
+                        return (
+                            <Col key={`${label.toLowerCase()}FilterColumn`}>
+                                <Filter key={`${label.toLowerCase()}Filter`} filterComponentData={filterComponentData} />
+                            </Col>
+                        );
+                    })
+                }
             </Row>
             <div className="timeline">
-                { projects.map(project => <Project key={project.name} projectData={project} />) }
+                { filterTopics(filtersCurrentlyInUse).map(project => <Project key={project.name} projectData={project} />) }
             </div>
         </>
     );
